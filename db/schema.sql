0e40bbdbb9175ade35b9aaa6799e9f51082a9271
EricaBabb <ecamilebabb@gmail.com>
2021-07-01 21:29:30 -0400

    voters table

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,17 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
+
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+--   tells date and time when the voter registered
+-- In SQL, a DATETIME field's value will look something like 2021-01-01 13:00:00.
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+--   DEFAULT: If you don't specify NOT NULL, then a field could potentially be NULL if that value isn't provided in an INSERT statement. With DEFAULT, however, you can specify what the value should be if no value is provided.
+);
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
